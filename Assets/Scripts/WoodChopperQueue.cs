cda18d7bb9b4b52ee6e6787a7c544400783153a1
Andries <61500390+Emerald001@users.noreply.github.com>
2021-12-02 17:52:35 +0100

    Tried some things

--- Assets/Scripts/WoodChopperQueue.cs
+++ Assets/Scripts/WoodChopperQueue.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class WoodChopperQueue : MonoBehaviour
 {
-    
+    public GameObject NoteHolder;
+
+    public List<Action> Actions = new List<Action>();
 
     public Queue<Action> Sequence = new Queue<Action>();
 
-    void MakeRoad()
-    {
-        for (int i = 0; i < length; i++)
-        {
+    private void Start() {
+        //foreach (Action A in NoteHolder.transform.GetComponentInChildren<Action>()) {
+        //    Actions.Add(A);
+        //}
+    }
 
+    private List<Action> MakeRoad(Action actionToAchieve)
+    {
+        var currentAction = actionToAchieve;
+
+        var bestList = new List<Action>();
+        var bestCost = 0f;
+
+        while (currentAction.Preconditions != null) {
+            var currentCost = 0f;
+            var list = new List<Action>();
+
+            for (int j = 0; j < Actions.Count; j++)
+            {
+                if (Actions[j] == actionToAchieve)
+                    continue;
+
+                if (Actions[j].Effects != actionToAchieve.Preconditions)
+                    continue;
+
+                currentCost += Actions[j].actionCost;
+
+            }
         }
+
+        if(bestList.Count != 0)
+            return bestList;
+        return null;
     }
 }
